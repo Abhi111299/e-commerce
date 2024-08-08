bdf996dfd56d2c17e008d13cec4b7c2029e7bb44
Abhishek Yadav <abhishek.yadav@debutinfotech.com>
2024-08-08 18:01:19 +0530

    delete user api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -175,4 +175,16 @@ exports.updateUserRole = (catchAsyncErrors(async(req, res, next)=>{
     res.status(200).json({success: true, message: "User role updated successfully"});
 }))
 
+exports.deleteUser = (catchAsyncErrors(async(req, res, next)=>{
+    const user = await User.findById(req.params.id); 
+    console.log("=======>", user);
+    if(!user){
+        return next(new ErrorHandler("User not found", 404));
+    }
+
+    await User.findByIdAndDelete(user);
+
+    res.status(200).json({success: true, message: "User Deleted successfully"});
+}))
+
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { registerUser, loginUser, logout, getUserData, forgotPassword, resetPassword, updatePassword, updateProfile, getAllUsers, getSingleUserById, updateUserRole } = require('../controllers/userController');
+const { registerUser, loginUser, logout, getUserData, forgotPassword, resetPassword, updatePassword, updateProfile, getAllUsers, getSingleUserById, updateUserRole, deleteUser } = require('../controllers/userController');
 const { isAuthenticatedUser, authorizeRole } = require('../middleware/auth');
 const router = express.Router();
 
@@ -10,6 +10,7 @@ router.route("/password/reset/:token").put(resetPassword);
 router.route("/password/update").put(isAuthenticatedUser, updatePassword);
 router.route("/profile/update").put(isAuthenticatedUser, updateProfile);
 router.route("/admin/role/update").put(isAuthenticatedUser, authorizeRole('user'), updateUserRole);
+router.route("/admin/delete/user/:id").delete(isAuthenticatedUser, authorizeRole('user'), deleteUser);
 router.route("/logout").get(logout);
 router.route("/admin/all_users").get(isAuthenticatedUser, authorizeRole('user'), getAllUsers);
 router.route("/admin/single_user/:id").get(isAuthenticatedUser, authorizeRole('user'), getSingleUserById);
